3e046f7935f043fc9ae613dd7b8449dc35825687
chirag <chirag.dev18@gmail.com>
2022-03-12 21:25:03 +0530

    header + new arrivals skeleton

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+// Dashboard
+import '../styles/dashboard/header.css'
+import '../styles/dashboard/dashboard.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/dashboard/index.js
@@ -0,0 +1,59 @@
+function Dashboard() {
+  var today = new Date()
+  var dd = String(today.getDate()).padStart(2, '0')
+  var mm = String(today.getMonth() + 1).padStart(2, '0') //January is 0!
+  var yyyy = today.getFullYear()
+  return (
+    <>
+      {/* HEADER */}
+      <div className="dashboard-header">
+        <div className="dashboard-header-time">
+          <p>{dd}&nbsp;</p>
+          <p>{mm}&nbsp;</p>
+
+          <p>{yyyy}&nbsp;</p>
+        </div>
+        <div>
+          <div className="dashboard-header-search"></div>
+          <div className="dashboard-header-profile"></div>
+        </div>
+
+        {/* <input type="search" placeholder="Search a book or an author" /> */}
+      </div>
+
+      {/* MAIN */}
+      <main className="dashboard">
+        <div className="dashboard-title">
+          <h2>New Arrivals</h2>
+          <a href="#">view all {'>'}</a>
+        </div>
+
+        {/* New Books */}
+        <div className="dashboard-new-arrivals">
+          <div className="new-arrivals-container">
+            <img src="/vercel.svg" alt="Book Image" />
+            <p>Book Name</p>
+            <p>Author Name</p>
+          </div>
+          <div className="new-arrivals-container">
+            <img src="/vercel.svg" alt="Book Image" />
+            <p>Book Name</p>
+            <p>Author Name</p>
+          </div>
+          <div className="new-arrivals-container">
+            <img src="/vercel.svg" alt="Book Image" />
+            <p>Book Name</p>
+            <p>Author Name</p>
+          </div>
+          <div className="new-arrivals-container">
+            <img src="/vercel.svg" alt="Book Image" />
+            <p>Book Name</p>
+            <p>Author Name</p>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Dashboard
