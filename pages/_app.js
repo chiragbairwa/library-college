38587f11519cbccfb60a48b4a51e8bbecc0315b4
chirag <chirag.dev18@gmail.com>
2022-05-01 15:18:40 +0530

    Auth-Page

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 // Dashboard
 import '../styles/dashboard/header.css'
 import '../styles/dashboard/dashboard.css'
+import './login/login.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,5 +1,52 @@
 function Login() {
-  return <></>
+  return (
+    <div className="auth-container">
+      {/* <img src="/vercel.svg"></img> */}
+      <div className="image">LOGO</div>
+      {/* Create Account button*/}
+      <a href="#" className="btn auth-options create-account-btn">
+        Create account
+      </a>
+
+      {/* Login button */}
+      <a href="#" className="btn auth-options login-btn">
+        Login
+      </a>
+
+      {/* Additional Options */}
+      <h5>Or continue with</h5>
+
+      <div className="additional-option-container">
+        <a href="#" className="btn facebook-btn">
+          Facebook
+        </a>
+        <a href="#" className="btn">
+          Google
+        </a>
+      </div>
+    </div>
+  )
+}
+
+function Register() {
+  return (
+    <>
+      <img src="/vercel.svg"></img>
+
+      {/* Create Account button*/}
+      <a>Create account</a>
+
+      {/* Login button */}
+      <a>Login</a>
+
+      {/* Additional Options */}
+      <h5>Or continue with</h5>
+
+      <a>Facebook</a>
+
+      <a>Google</a>
+    </>
+  )
 }
 
 export default Login
