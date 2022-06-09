f1a5cef3405077d4c397ab5d983e13966fed31f9
chirag <chirag.dev18@gmail.com>
2022-06-09 20:26:23 +0530

    Content of Shelf Collection -Featured -Owned -Reviews

--- pages/collection/index.js
+++ pages/collection/index.js
@@ -7,8 +7,9 @@ const Collection = () => {
   const [owned, setOwned] = useState({})
   const [reviews, setReviews] = useState({})
 
-  let selected = 0
-  useEffect(() => {}, [feature, owned, reviews])
+  const [selected, setSelected] = useState(1)
+
+  useEffect(() => {}, [selected, feature, owned, reviews])
 
   return (
     <div className="book-collection">
@@ -19,13 +20,14 @@ const Collection = () => {
         <p>Shelves Collection</p>
       </div>
       <div className="collection-tabs">
+        {/* Feature tab */}
         <a
           href="#"
           className="collection-tab feature-tab"
           style={feature}
           onClick={() => {
             if (selected !== 1) {
-              selected = 1
+              setSelected(1)
 
               setFeature(borderLine)
               setOwned({})
@@ -35,13 +37,15 @@ const Collection = () => {
         >
           Featured
         </a>
+
+        {/* Owned tab */}
         <a
           href="#"
           className="collection-tab Owned-tab"
           style={owned}
           onClick={() => {
             if (selected !== 2) {
-              selected = 2
+              setSelected(2)
 
               setFeature({})
               setOwned(borderLine)
@@ -51,13 +55,15 @@ const Collection = () => {
         >
           Owned
         </a>
+
+        {/* Reviews tab */}
         <a
           href="#"
           className="collection-tab reviews-tab"
           style={reviews}
           onClick={() => {
             if (selected !== 3) {
-              selected = 3
+              setSelected(3)
               setFeature({})
               setOwned({})
               setReviews(borderLine)
@@ -67,8 +73,141 @@ const Collection = () => {
           Reviews
         </a>
       </div>
+
+      <div className="content">
+        {selected == 1 ? (
+          // Featured Content
+          <Featured />
+        ) : selected == 2 ? (
+          // Owned Content
+          <Owned />
+        ) : (
+          // Reviews Content
+          <Reviews />
+        )}
+      </div>
     </div>
   )
 }
 
+const Featured = () => {
+  return (
+    <>
+      <h4>Reading Now</h4>
+      <span>5 books</span>
+
+      {/* Reading Books/ issued  */}
+      <div className="dashboard-new-arrivals">
+        <div className="new-arrivals-container">
+          <img src="/vercel.svg" alt="Book Image" />
+          <p>Book Name</p>
+          <p>Author Name</p>
+        </div>
+        <div className="new-arrivals-container">
+          <img src="/vercel.svg" alt="Book Image" />
+          <p>Book Name</p>
+          <p>Author Name</p>
+        </div>
+        <div className="new-arrivals-container">
+          <img src="/vercel.svg" alt="Book Image" />
+          <p>Book Name</p>
+          <p>Author Name</p>
+        </div>
+        <div className="new-arrivals-container">
+          <img src="/vercel.svg" alt="Book Image" />
+          <p>Book Name</p>
+          <p>Author Name</p>
+        </div>
+      </div>
+
+      {/* Favourites */}
+      <h4>Favourites</h4>
+      <span>5 books</span>
+
+      <div className="dashboard-new-arrivals">
+        <div className="new-arrivals-container">
+          <img src="/vercel.svg" alt="Book Image" />
+          <p>Book Name</p>
+          <p>Author Name</p>
+        </div>
+        <div className="new-arrivals-container">
+          <img src="/vercel.svg" alt="Book Image" />
+          <p>Book Name</p>
+          <p>Author Name</p>
+        </div>
+        <div className="new-arrivals-container">
+          <img src="/vercel.svg" alt="Book Image" />
+          <p>Book Name</p>
+          <p>Author Name</p>
+        </div>
+        <div className="new-arrivals-container">
+          <img src="/vercel.svg" alt="Book Image" />
+          <p>Book Name</p>
+          <p>Author Name</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const Owned = () => {
+  return (
+    <>
+      <h4>3 books</h4>
+
+      {/* Reading Books/ issued  */}
+      <div className="owned-cards">
+        <div className="owned-card">
+          <img src="/vercel.svg" alt="Book Image" />
+          <p>Book Name</p>
+        </div>
+        <div className="owned-card">
+          <img src="/vercel.svg" alt="Book Image" />
+          <p>Book Name</p>
+        </div>
+        <div className="owned-card">
+          <img src="/vercel.svg" alt="Book Image" />
+          <p>Book Name</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const Reviews = () => {
+  return (
+    <>
+      <h4>5 Reviews</h4>
+      {/* Reviews */}
+      <div className="reviews-cards">
+        <div className="reviews-card">
+          <div className="div-image"></div>
+
+          <div className="reviews-card-info">
+            <p>Book Name</p>
+            <p>******25/10/2002</p>
+            <p>lorem ipsum</p>
+          </div>
+        </div>
+        <div className="reviews-card">
+          <div className="div-image"></div>
+
+          <div className="reviews-card-info">
+            <p>Book Name</p>
+            <p>******25/10/2002</p>
+            <p>lorem ipsum</p>
+          </div>
+        </div>
+        <div className="reviews-card">
+          <div className="div-image"></div>
+          <div className="reviews-card-info">
+            <p>Book Name</p>
+            <p>******25/10/2002</p>
+            <p>lorem ipsum</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
 export default Collection
